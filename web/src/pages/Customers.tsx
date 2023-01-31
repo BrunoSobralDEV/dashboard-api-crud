09ba9d8cc290a5a8f630ec6f63a048a716883c53
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2023-01-31 17:18:03 -0300

    feat: bootstrap modal add to new constumer

--- web/src/pages/Customers.tsx
+++ web/src/pages/Customers.tsx
@@ -1,4 +1,7 @@
 import { FormEvent, useEffect, useState } from 'react'
+import { Pencil, Trash, MagnifyingGlass, UserPlus } from "phosphor-react";
+import {Alert, Button, Modal, Form} from 'react-bootstrap';
+
 import { Loading } from '../components/Loading';
 
 interface Customer {
@@ -18,6 +21,11 @@ export function Customers() {
   
   const [userName, setUserName] = useState('');
   
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   let indexOfLastUser = currentPage * usersPerPage;
   let indexOfFirstUser = indexOfLastUser - usersPerPage;
   
@@ -60,20 +68,31 @@ export function Customers() {
       <h1>Lista de Clientes - JSONServer</h1>
       
       <div style={{display: "flex", justifyContent: "space-between", padding: "40px 0"}}>
-        <form onSubmit={handleFilterCustomers}>
-          <input 
+        <Form onSubmit={handleFilterCustomers}>
+          <Form.Group className="mb-6">
+            <Form.Control 
+              type="text" 
+              placeholder='Busque pelo nome ou e-mail' 
+              value={userName}
+              onChange={event => setUserName(event.target.value)}
+            />
+          </Form.Group>
+          {/* <input 
             style={{width: "220px"}}
             type="text" 
             placeholder='Busque pelo nome ou e-mail' 
             value={userName}
             onChange={event => setUserName(event.target.value)}
-          />
-          <button >Buscar</button>
-        </form>
+          /> */}
+          <Button className="mb-3" variant="secondary">
+            <MagnifyingGlass size={16} /> Buscar
+          </Button>
+          {/* <button ><MagnifyingGlass size={16} /> Buscar</button> */}
+        </Form>
 
-        <button>+ Cadastrar</button>
+        <button onClick={handleShow}>Cadastrar</button>
       </div>
-
+      
       <table>
         <tbody>
           <tr>
@@ -82,6 +101,7 @@ export function Customers() {
             <th>Telefone</th>
             <th>Endereço</th>
             <th>CPF</th>
+            <th>Editar | Deletar</th>
           </tr>
           {customers.slice(indexOfFirstUser, indexOfLastUser).map((customer, index) => {
             return (
@@ -91,7 +111,9 @@ export function Customers() {
                 <td style={{padding: "10px"}}>{customer.phone}</td>
                 <td style={{padding: "10px"}}>{customer.address}</td>
                 <td style={{padding: "10px"}}>{customer.cpf}</td>
-                <td style={{padding: "10px"}}>Edtar | Deletar</td>
+                <td style={{padding: "10px"}}>
+                  <Pencil size={24} /> | <Trash size={24} />
+                </td>
               </tr>
             )
           })}
@@ -102,6 +124,60 @@ export function Customers() {
         {/* <button onClick={() => setCurrentPage(2)}>2</button>
         <button onClick={() => setCurrentPage(3)}>3</button> */}
       </>
+      
+      <Modal show={show} onHide={handleClose} backdrop="static">
+      <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Nome"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@exemplo.com"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
+              <Form.Label>Telefone</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="(DDD) 00000-0000"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
+              <Form.Label>Endereço</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Endereço"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
+              <Form.Label>CPF</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="000.000.000-00"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Salvar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
